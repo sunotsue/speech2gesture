ba6cc2f1b4c8ee09f162293e46f44ce24d8f25e9
Su Park <32600584+sunotsue@users.noreply.github.com>
2022-07-14 16:40:49 -0400

    Add files via upload

---
+++ trainer_chooser.py
@@ -0,0 +1,26 @@
+import model.trainer
+
+import pdb
+def trainer_chooser(args):
+  trainer_name = 'model.trainer.Trainer'
+  if 'Joint' in args.model:
+    trainer_name += 'Joint'
+  if 'Late' in args.model:
+    trainer_name += 'Late'
+  if 'Gest' in args.model:
+    trainer_name += 'Gest'
+  if 'Cluster' in args.model:
+    trainer_name += 'Cluster'
+  if args.gan:
+    trainer_name += 'GAN'
+  if 'Style' in args.model:
+    trainer_name += 'Style'
+  if 'Classifier' in args.model:
+    trainer_name += 'Classifier'
+
+  try:
+    eval(trainer_name)
+  except:
+    raise '{} trainer not defined'.format(trainer_name)
+  print('{} selected'.format(trainer_name))
+  return eval(trainer_name)
